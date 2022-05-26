4f9c4602db9b0d188ed6fecfcd33249ce8d83cae
mateib47 <m.bucur@student.utwente.nl>
2022-05-26 22:58:37 +0200

    init repository, cleanup, add topbar, icon

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import './topbar.scss'
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+        <div className="wrapper">
+            <h1>drum machine.</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
