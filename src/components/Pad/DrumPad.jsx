43b720869a951a0735965c1de4fa1449d0a62a01
mateib47 <m.bucur@student.utwente.nl>
2022-05-29 23:59:26 +0200

    add play audio

--- src/components/Pad/DrumPad.jsx
+++ src/components/Pad/DrumPad.jsx
@@ -1,10 +1,21 @@
+import { startTransition } from "react";
 import "./pad.scss";
+import { useEffect } from "react";
 
 const DrumPad = ({ id, text }) => {
+  const muie = [];
+    const startSound = () => {
+
+      const sound = document.getElementById(text);
+      sound.currentTime = 0;
+      sound.play();
+    }
+
+
   return (
-    <div className="drum-pad" id={id}>
+    <div className="drum-pad" id={id} onClick={()=>{startSound()}} >
       {text}
-      <audio src="../../public/assets/audios/punch.mp3"></audio>
+      <audio src="../../../public/assets/audios/punch.mp3" className="clip" id={text}></audio>
     </div>
   );
 };
